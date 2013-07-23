20637c58384b32742a8a94bbace63d02d0c22d35
LONELY-WOLF <zvenayte@pisem.net>
2013-07-23 08:29:15 +0400

    some more code for core driver

--- src/s3c64fb.c
+++ src/s3c64fb.c
@@ -310,8 +310,460 @@ fail:
 static void
 S3C64FBAccelInit(ScreenPtr pScreen)
 {
+	ScrnInfoPtr pScrn = xf86Screens[pScreen->myNum];
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+
+	if (!pS3C64FB->NoAccel) {
+		INFO_MSG("Initializing the \"%s\" sub-module ...", SUB_MODULE_G2D);
+		pS3C64FB->pS3C64FBEXA = InitG2DEXA(pScreen, pScrn, pS3C64FB->drmFD);
+		if (pS3C64FB->pS3C64FBEXA) {
+			INFO_MSG("Successfully initialized the \"%s\" sub-module",
+					SUB_MODULE_G2D);
+		} else {
+			INFO_MSG("Could not initialize the \"%s\" sub-module",
+					SUB_MODULE_G2D);
+			pS3C64FB->NoAccel = TRUE;
+		}
+	}
+
+	if (!pS3C64FB->pS3C64FBEXA) {
+		pS3C64FB->pS3C64FBEXA = InitNullEXA(pScreen, pScrn, pS3C64FB->drmFD);
+	}
+
+	if (pS3C64FB->dri && pS3C64FB->pS3C64FBEXA) {
+		pS3C64FB->dri = S3C64FBDRI2ScreenInit(pScreen);
+	} else {
+		pS3C64FB->dri = FALSE;
+	}
+
+	if (S3C64FBVideoScreenInit(pScreen)) {
+		INFO_MSG("Initialized XV");
+	} else {
+		ERROR_MSG("Could not initialize XV");
+	}
+}
+
+/**
+ * The driver's ScreenInit() function.  Fill in pScreen, map the frame buffer,
+ * save state, initialize the mode, etc.
+ */
+static Bool
+S3C64FBScreenInit(SCREEN_INIT_ARGS_DECL)
+{
+	ScrnInfoPtr pScrn = xf86Screens[pScreen->myNum];
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+	VisualPtr visual;
+	xf86CrtcConfigPtr xf86_config;
+	int i;
+
+	TRACE_ENTER();
+
+	/* Allocate and map memory areas we need */
+	if (!S3C64FBMapMem(pScrn))
+		return FALSE;
+
+	xf86_config = XF86_CRTC_CONFIG_PTR(pScrn);
+
+	/* need to point to new screen on server regeneration */
+	for (i = 0; i < xf86_config->num_crtc; i++)
+		xf86_config->crtc[i]->scrn = pScrn;
+	for (i = 0; i < xf86_config->num_output; i++)
+		xf86_config->output[i]->scrn = pScrn;
+
+	/*
+	 * The next step is to setup the screen's visuals, and initialize the
+	 * framebuffer code.  In cases where the framebuffer's default
+	 * choices for things like visual layouts and bits per RGB are OK,
+	 * this may be as simple as calling the framebuffer's ScreenInit()
+	 * function.  If not, the visuals will need to be setup before calling
+	 * a fb ScreenInit() function and fixed up after.
+	 *
+	 * For most PC hardware at depths >= 8, the defaults that fb uses
+	 * are not appropriate.  In this driver, we fixup the visuals after.
+	 */
+
+	/*
+	 * Reset the visual list.
+	 */
+
+	miClearVisualTypes();
+	if (!miSetVisualTypes(pScrn->depth, miGetDefaultVisualMask(pScrn->depth),
+			pScrn->rgbBits, pScrn->defaultVisual)) {
+		ERROR_MSG("Cannot initialize the visual type for %d bits per pixel!",
+				pScrn->bitsPerPixel);
+		goto fail;
+	}
+
+	if (!miSetPixmapDepths()) {
+		ERROR_MSG("Cannot initialize the pixmap depth!");
+		goto fail;
+	}
+
+	/* Initialize some generic 2D drawing functions: */
+	if (!fbScreenInit(pScreen, s3c64fb_bo_map(pS3C64FB->scanout),
+			pScrn->virtualX, pScrn->virtualY,
+			pScrn->xDpi, pScrn->yDpi, pScrn->displayWidth,
+			pScrn->bitsPerPixel)) {
+		ERROR_MSG("fbScreenInit() failed!");
+		goto fail;
+	}
+
+	/* Fixup RGB ordering: */
+	visual = pScreen->visuals + pScreen->numVisuals;
+	while (--visual >= pScreen->visuals) {
+		if ((visual->class | DynamicClass) == DirectColor) {
+			visual->offsetRed = pScrn->offset.red;
+			visual->offsetGreen = pScrn->offset.green;
+			visual->offsetBlue = pScrn->offset.blue;
+			visual->redMask = pScrn->mask.red;
+			visual->greenMask = pScrn->mask.green;
+			visual->blueMask = pScrn->mask.blue;
+		}
+	}
+
+	/* Continue initializing the generic 2D drawing functions after fixing the
+	 * RGB ordering:
+	 */
+	if (!fbPictureInit(pScreen, NULL, 0)) {
+		ERROR_MSG("fbPictureInit() failed!");
+		goto fail;
+	}
+
+	/* Set the initial black & white colormap indices: */
+	xf86SetBlackWhitePixels(pScreen);
+
+	/* Initialize external sub-modules for EXA now, this has to be before
+	 * miDCInitialize() otherwise stacking order for wrapped ScreenPtr fxns
+	 * ends up in the wrong order.
+	 */
+	S3C64FBAccelInit(pScreen);
+
+	/* Initialize backing store: */
+	xf86SetBackingStore(pScreen);
+
+	/* Cause the cursor position to be updated by the mouse signal handler: */
+	xf86SetSilkenMouse(pScreen);
+
+	/* Initialize the cursor: */
+	miDCInitialize(pScreen, xf86GetPointerScreenFuncs());
+
+	if (pS3C64FB->HWCursor) {
+		if (!drmmode_cursor_init(pScreen)) {
+			ERROR_MSG("Hardware cursor initialization failed");
+			pS3C64FB->HWCursor = FALSE;
+		}
+	}
+
+	/* XXX -- Is this the right place for this?  The Intel i830 driver says:
+	 * "Must force it before EnterVT, so we are in control of VT..."
+	 */
+	pScrn->vtSema = TRUE;
+
+	/* Take over the virtual terminal from the console, set the desired mode,
+	 * etc.:
+	 */
+	S3C64FBEnterVT(VT_FUNC_ARGS(0));
+
+	/* Set the desired mode(s): */
+	if (!xf86SetDesiredModes(pScrn)) {
+		ERROR_MSG("xf86SetDesiredModes() failed!");
+		goto fail;
+	}
+
+	/* Do some XRandR initialization: */
+	if (!xf86CrtcScreenInit(pScreen)) {
+		ERROR_MSG("xf86CrtcScreenInit() failed!");
+		goto fail;
+	}
+
+	if (has_rotation(pS3C64FB)) {
+		xf86RandR12SetRotations(pScreen, RR_Rotate_0 | RR_Rotate_90 |
+				RR_Rotate_180 | RR_Rotate_270 | RR_Reflect_X | RR_Reflect_Y);
+	} else {
+#if XF86_CRTC_VERSION < 4
+		WARNING_MSG("rotation not supported by XF86_CRTC_VERSION version: %d",
+				XF86_CRTC_VERSION);
+#endif
+	}
+
+	if (!miCreateDefColormap(pScreen)) {
+		ERROR_MSG("Cannot create colormap!");
+		goto fail;
+	}
+
+	if (!xf86HandleColormaps(pScreen, 256, 8, S3C64FBLoadPalette, NULL,
+			CMAP_PALETTED_TRUECOLOR)) {
+		ERROR_MSG("xf86HandleColormaps() failed!");
+		goto fail;
+	}
+
+	/* Setup power management: */
+	xf86DPMSInit(pScreen, xf86DPMSSet, 0);
+
+	pScreen->SaveScreen = xf86SaveScreen;
+
+	/* Wrap some screen functions: */
+	wrap(pS3C64FB, pScreen, CloseScreen, S3C64FBCloseScreen);
+	wrap(pS3C64FB, pScreen, CreateScreenResources, S3C64FBCreateScreenResources);
+	wrap(pS3C64FB, pScreen, BlockHandler, S3C64FBBlockHandler);
+
+	drmmode_screen_init(pScrn);
+
+	TRACE_EXIT();
+	return TRUE;
+
+fail:
+	TRACE_EXIT();
+	return FALSE;
+}
+
+
+static void
+S3C64FBLoadPalette(ScrnInfoPtr pScrn, int numColors, int *indices,
+		LOCO * colors, VisualPtr pVisual)
+{
+	TRACE_ENTER();
+	TRACE_EXIT();
+}
+
+
+/**
+ * The driver's CloseScreen() function.  This is called at the end of each
+ * server generation.  Restore state, unmap the frame buffer (and any other
+ * mapped memory regions), and free per-Screen data structures (except those
+ * held by pScrn).
+ */
+static Bool
+S3C64FBCloseScreen(CLOSE_SCREEN_ARGS_DECL)
+{
+	ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+
+	TRACE_ENTER();
+
+	drmmode_screen_fini(pScrn);
+
+	if (pScrn->vtSema == TRUE) {
+		S3C64FBLeaveVT(VT_FUNC_ARGS(0));
+	}
+
+	if (pS3C64FB->pS3C64FBEXA) {
+		if (pS3C64FB->pS3C64FBEXA->CloseScreen) {
+			pS3C64FB->pS3C64FBEXA->CloseScreen(CLOSE_SCREEN_ARGS);
+		}
+	}
+
+	if (pS3C64FB->dri) {
+		S3C64FBDRI2CloseScreen(pScreen);
+	}
+
+	S3C64FBVideoCloseScreen(pScreen);
+
+	S3C64FBUnmapMem(pScrn);
+
+	pScrn->vtSema = FALSE;
+
+	unwrap(pS3C64FB, pScreen, CloseScreen);
+	unwrap(pS3C64FB, pScreen, BlockHandler);
+	unwrap(pS3C64FB, pScreen, CreateScreenResources);
+
+	TRACE_EXIT();
+
+	return (*pScreen->CloseScreen)(CLOSE_SCREEN_ARGS);
+}
 
 
 
+/**
+ * Adjust the screen pixmap for the current location of the front buffer.
+ * This is done at EnterVT when buffers are bound as long as the resources
+ * have already been created, but the first EnterVT happens before
+ * CreateScreenResources.
+ */
+static Bool
+S3C64FBCreateScreenResources(ScreenPtr pScreen)
+{
+	ScrnInfoPtr pScrn = xf86Screens[pScreen->myNum];
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
 
+	swap(pS3C64FB, pScreen, CreateScreenResources);
+	if (!(*pScreen->CreateScreenResources) (pScreen))
+		return FALSE;
+	swap(pS3C64FB, pScreen, CreateScreenResources);
+
+	return TRUE;
+}
+
+
+static void
+S3C64FBBlockHandler(BLOCKHANDLER_ARGS_DECL)
+{
+	SCREEN_PTR(arg);
+	ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+
+	swap(pS3C64FB, pScreen, BlockHandler);
+	(*pScreen->BlockHandler) (BLOCKHANDLER_ARGS);
+	swap(pS3C64FB, pScreen, BlockHandler);
+
+	/* TODO S3C64FBVideoBlockHandler(), etc.. */
+}
+
+
+
+/**
+ * The driver's SwitchMode() function.  Initialize the new mode for the
+ * Screen.
+ */
+static Bool
+S3C64FBSwitchMode(SWITCH_MODE_ARGS_DECL)
+{
+	SCRN_INFO_PTR(arg);
+	return xf86SetSingleMode(pScrn, mode, RR_Rotate_0);
+}
+
+
+
+/**
+ * The driver's AdjustFrame() function.  For cases where the frame buffer is
+ * larger than the monitor resolution, this function can pan around the frame
+ * buffer within the "viewport" of the monitor.
+ */
+static void
+S3C64FBAdjustFrame(ADJUST_FRAME_ARGS_DECL)
+{
+	SCRN_INFO_PTR(arg);
+	drmmode_adjust_frame(pScrn, x, y);
+}
+
+
+
+/**
+ * The driver's EnterVT() function.  This is called at server startup time, and
+ * when the X server takes over the virtual terminal from the console.  As
+ * such, it may need to save the current (i.e. console) HW state, and set the
+ * HW state as needed by the X server.
+ */
+static Bool
+S3C64FBEnterVT(VT_FUNC_ARGS_DECL)
+{
+	SCRN_INFO_PTR(arg);
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+	int ret;
+
+	TRACE_ENTER();
+
+	ret = drmSetMaster(pS3C64FB->drmFD);
+	if (ret) {
+		ERROR_MSG("Cannot get DRM master: %s\n", strerror(ret));
+	}
+
+	if (!xf86SetDesiredModes(pScrn)) {
+		ERROR_MSG("xf86SetDesiredModes() failed!");
+		return FALSE;
+	}
+
+	TRACE_EXIT();
+	return TRUE;
+}
+
+
+
+/**
+ * The driver's LeaveVT() function.  This is called when the X server
+ * temporarily gives up the virtual terminal to the console.  As such, it may
+ * need to restore the console's HW state.
+ */
+static void
+S3C64FBLeaveVT(VT_FUNC_ARGS_DECL)
+{
+	SCRN_INFO_PTR(arg);
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+	int ret;
+
+	TRACE_ENTER();
+
+	ret = drmDropMaster(pS3C64FB->drmFD);
+	if (ret) {
+		WARNING_MSG("drmDropMaster failed: %s\n", strerror(errno));
+	}
+
+	TRACE_EXIT();
+}
+
+
+
+/**
+ * The driver's FreeScreen() function.  This is called at the server's end of
+ * life.  This should free any driver-allocated data that was allocated
+ * up-to-and-including an unsuccessful ScreenInit() call.
+ */
+static void
+S3C64FBFreeScreen(FREE_SCREEN_ARGS_DECL)
+{
+	SCRN_INFO_PTR(arg);
+	S3C64FBPtr pS3C64FB = S3C64FBPTR(pScrn);
+
+	TRACE_ENTER();
+
+	if (!pS3C64FB) {
+		/* This can happen if a Screen is deleted after Probe(): */
+		return;
+	}
+
+	if (pS3C64FB->pS3C64FBEXA) {
+		if (pS3C64FB->pS3C64FBEXA->FreeScreen) {
+			pS3C64FB->pS3C64FBEXA->FreeScreen(FREE_SCREEN_ARGS(pScrn));
+		}
+		free(pS3C64FB->pS3C64FBEXA);
+	}
+
+	s3c64fb_device_del(pS3C64FB->dev);
+
+	S3C64FBCloseDRMMaster(pScrn);
+
+	S3C64FBFreeRec(pScrn);
+
+	TRACE_EXIT();
+}
+
+#ifdef XSERVER_PLATFORM_BUS
+static Bool
+S3C64FBPlatformProbe(DriverPtr drv, int entity_num, int flags,
+                   struct xf86_platform_device *dev, intptr_t match_data)
+{
+	ScrnInfoPtr pScrn = NULL;
+	Bool foundScreen = FALSE;
+	char *busid = xf86_get_platform_device_attrib(dev, ODEV_ATTRIB_BUSID);
+	int fd;
+
+	fd = drmOpen(NULL, busid);
+	if (fd != -1) {
+		pScrn = xf86AllocateScreen(drv, 0);
+		if (!pScrn) {
+			EARLY_ERROR_MSG("Cannot allocate a ScrnInfoPtr");
+			return FALSE;
+		}
+
+		xf86AddEntityToScreen(pScrn, entity_num);
+		foundScreen = TRUE;
+
+		pScrn->driverVersion = S3C64FB_VERSION;
+		pScrn->driverName    = (char *)S3C64FB_DRIVER_NAME;
+		pScrn->name          = (char *)S3C64FB_NAME;
+		pScrn->Probe         = S3C64FBProbe;
+		pScrn->PreInit       = S3C64FBPreInit;
+		pScrn->ScreenInit    = S3C64FBScreenInit;
+		pScrn->SwitchMode    = S3C64FBSwitchMode;
+		pScrn->AdjustFrame   = S3C64FBAdjustFrame;
+		pScrn->EnterVT       = S3C64FBEnterVT;
+		pScrn->LeaveVT       = S3C64FBLeaveVT;
+		pScrn->FreeScreen    = S3C64FBFreeScreen;
+
+		drmClose(fd);
+	}
+
+	return foundScreen;
+}
+#endif
 
